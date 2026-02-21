98a9518810bdd4e4f124d1217d20d111b6c28806
sesamex <sesamex@example.com>
2026-02-21 18:30:53 +0800

    feat:狗子展示功能包

---
+++ show_dog/launch/display.launch.py
@@ -0,0 +1,20 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    urdf_file = os.path.join(
+        get_package_share_directory('show_dog'),
+        'urdf', 'dog.urdf'
+    )
+    with open(urdf_file, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+        ),
+    ])
